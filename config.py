47e7b7d230bfbc70388c1354eb6b2513c6079263
nurtynovaminsahip-debug <nurtynovaminsahip@gmail.com>
2026-05-24 20:33:57 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+ADMIN_GROUP_ID = int(os.environ.get('ADMIN_GROUP_ID', '0'))
+OWNER_ID = int(os.environ.get('OWNER_ID', '0'))
+
+_ch = os.environ.get('CHANNEL_USERNAME', '@TrasnferMarketBrawlStars').strip()
+try:
+    CHANNEL_USERNAME = int(_ch)
+except ValueError:
+    CHANNEL_USERNAME = _ch
+
+COOLDOWN = 30
+LOG_PAGE_SIZE = 10
